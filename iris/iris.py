8966f0f1f931cf237940823be474fabb8c08dd96
qian <s223554@gmail.com>
2017-09-09 23:09:11 -0400

    basic visualization and statistics

--- iris/iris.py
+++ iris/iris.py
@@ -1,14 +1,25 @@
 import csv
 import pandas as pd
 import numpy as np
+import matplotlib
 
-with open('iris.data','r') as irisfile:
+matplotlib.use('TkAgg')
+
+import matplotlib.pyplot as plt
+
+plt.interactive(True)
+
+
+with open('iris/iris.data','r') as irisfile:
     file = csv.reader(irisfile)
     for row in file:
         print('.'.join(row))
 
-irisdata = pd.read_csv('iris.data')
+irisdata = pd.read_csv('iris/iris.data')
 irisdata.columns = ['lsepal','wsepal','lpetal','wpetal','class']
 
 # build up a pipeline for data visualization.
-print(np.sum(irisdata['lsepal']))
+#print(np.sum(irisdata['lsepal']))
+plt.scatter(irisdata['lsepal'],irisdata['wsepal'])
+np.percentile(irisdata['lsepal'],25)
+plt.boxplot(irisdata['lsepal'])
