c5a3b7dd08b436666c5cfe937068baf08c468dcd
s223554 <s223554@gmail.com>
2017-09-11 23:12:57 -0400

    basic ML classification.

--- iris/iris.py
+++ iris/iris.py
@@ -1,4 +1,4 @@
-import csv
+
 import pandas as pd
 import numpy as np
 import matplotlib
@@ -11,10 +11,10 @@ import matplotlib.pyplot as plt
 plt.interactive(True)
 
 
-with open('iris/iris.data','r') as irisfile:
-    file = csv.reader(irisfile)
-    for row in file:
-        print('.'.join(row))
+# with open('iris/iris.data','r') as irisfile:
+#     file = csv.reader(irisfile)
+#     for row in file:
+#         print('.'.join(row))
 
 irisdata = pd.read_csv('iris/iris.data')
 irisdata.columns = ['lsepal','wsepal','lpetal','wpetal','class']
@@ -33,4 +33,26 @@ sns.FacetGrid(irisdata,hue='class',size=7).map(plt.scatter,'lsepal','wsepal').ad
 # view boxplot of an attribute. Add individual points.
 sns.boxplot(x='class',y='lsepal',data=irisdata)
 sns.stripplot(x='class',y='lsepal',data = irisdata,jitter = True, edgecolor = 'gray')
+# feature distributions
+sns.FacetGrid(irisdata,hue='class',size=7).map(sns.kdeplot,'lsepal').add_legend()
+sns.pairplot(irisdata,hue='class',size=2)
+sns.pairplot(irisdata,hue='class',size=2,diag_kind='kde')
 
+# ML part
+
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn import metrics
+
+train,test = train_test_split(irisdata,test_size=0.3)
+trainX = train[['lsepal','wsepal','lpetal','wpetal']]
+trainY = train[['class']]
+testX = test[['lsepal','wsepal','lpetal','wpetal']]
+testY = test[['class']]
+
+# logistic regression
+
+model = LogisticRegression()
+model.fit(trainX,trainY)
+prediction = model.predict(testX)
+metrics.accuracy_score(prediction,testY)
